bb75ee7268652679c8509dea1a3657352a0f5a7e
Jarvis <jarvismiranda@gmail.com>
2022-03-02 11:21:28 -0500

    creando olvide password

--- src/paginas/OlvidePassword.jsx
+++ src/paginas/OlvidePassword.jsx
@@ -1,13 +1,49 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import Alerta from "../components/Alerta";
+import axios from "axios";
 
 const OlvidePassword = () => {
+  const [email, setEmail] = useState("");
+  const [alerta, setAlerta] = useState({});
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    //Validando que se llene el campo email
+    if (email === "" || email.length < 6) {
+      return setAlerta({
+        msg: "El email es obligatorio",
+        error: true,
+      });
+    }
+    try {
+      const { data } = await axios.post(
+        `${import.meta.env.VITE_BACKEND_URL}/api/usuarios/olvide-password/`,
+        { email }
+      );
+      //console.log(data);
+      setAlerta({
+        msg: data.msg,
+        error: false,
+      });
+    } catch (error) {
+      setAlerta({
+        msg: error.response.data.msg,
+        error: true,
+      });
+    }
+    //setAlerta({});
+  };
   return (
     <>
       <h1 className="text-sky-600 font-black text-4xl text-center">
         Recupera tu acceso y no pierdas tus{" "}
         <span className="text-slate-700">Proyectos</span>
       </h1>
-      <form className="my-10 bg-white p-10 shadow rounded-md">
+      {alerta.msg && <Alerta alerta={alerta} />}
+      <form
+        className="my-10 bg-white p-10 shadow rounded-md"
+        onSubmit={handleSubmit}
+      >
         <div className="my-5">
           <label
             htmlFor="email"
@@ -20,6 +56,8 @@ const OlvidePassword = () => {
             type="email"
             placeholder="Email de registro"
             className="w-full mt-2 p-2 rounded-xl bg-gray-50"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
           />
         </div>
 
